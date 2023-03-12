b357ccb3d0698ab3d37407959383d9b7275c954a
nunu111 <nunu111za@hotmail.com>
2023-03-12 22:57:13 +0700

    Add HomePage

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -3,23 +3,37 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <div>
-      <div
-        style={{
-          textAlign: "center",
-          minHeight: "100vh",
-        }}
-      >
-        <h1 className="Topic">Upbeat</h1>
-        {/* <Link to="/"></Link> */}
-        <Link to="/Player">
-          <button
-            className="d-grid gap-2 col-6 mx-auto btn btn-primary"
-            style={{ marginTop: "10vh", fontSize: "10vh" }}
-          >
-            Play
-          </button>
-        </Link>
+    <div style={{ minHeight: "100vh", textAlign: "center" }}>
+      <div>
+        <h1 className="Topic">Upbeat!!!</h1>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            height: "50vh",
+          }}
+        >
+          <div class="d-grid gap-5 col-6 mx-auto">
+            <Link to="/Player" style={{ textDecoration: "none" }}>
+              <button
+                class="btn btn-primary"
+                type="button"
+                style={{ fontSize: "10vh" }}
+              >
+                Start Game
+              </button>
+            </Link>
+            <Link to="/Player" style={{ textDecoration: "none" }}>
+              <button
+                class="btn btn-primary"
+                type="button"
+                style={{ fontSize: "10vh" }}
+              >
+                How to play
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );

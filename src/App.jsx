9220a91d6819fcf2cd5d07e01dc9693d7e3a9dca
nunu111 <nunu111za@hotmail.com>
2023-03-12 22:36:36 +0700

    add example of page changer for group members

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Page/HomePage";
+import SelectPlayerPage from "./Page/SelectPlayerPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/Player" element={<SelectPlayerPage />} />
     </Routes>
   );
 }
--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
@@ -11,12 +12,14 @@ export default function HomePage() {
       >
         <h1 className="Topic">Upbeat</h1>
         {/* <Link to="/"></Link> */}
-        <button
-          className="d-grid gap-2 col-6 mx-auto btn btn-primary"
-          style={{ marginTop: "10vh", fontSize: "10vh" }}
-        >
-          Play
-        </button>
+        <Link to="/Player">
+          <button
+            className="d-grid gap-2 col-6 mx-auto btn btn-primary"
+            style={{ marginTop: "10vh", fontSize: "10vh" }}
+          >
+            Play
+          </button>
+        </Link>
       </div>
     </div>
   );

0c45b27c099695e2aef4eed295020becb6e7355d
Sirawich <Sirawich_Pin@cmu.ac.th>
2023-03-25 03:30:02 +0700

    add name of player that make easiler then before

--- src/Page/SelectPlayerPage.jsx
+++ src/Page/SelectPlayerPage.jsx
@@ -5,33 +5,31 @@ export default function SelectPlayerPage() {
   const [showOptions, setShowOptions] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
   const [players, setPlayers] = useState([]);
-  const Navigate = useNavigate();
+  const navigate = useNavigate();
   const row = 15;
   const col = 20;
+
+  const handleNameChange = (index, event) => {
+    const newPlayers = [...players];
+    newPlayers[index].name = event.target.value;
+    setPlayers(newPlayers);
+  };
 
   const PlayerAdder = () => {
     if (selectedOption === null) {
       alert("Please select an option");
     } else if (selectedOption === "custom") {
-      if (players < 2) alert("Player Should have more than 1");
-      else if (players > row * col) alert("Player Should have less than field");
+      if (players.length < 2) alert("Player should have at least 2 players");
+      else if (players.length > row * col)
+        alert("Too many players for the field");
       else {
-        const playerData = [];
-        for (let i = 0; i < players[0]; i++) {
-          const playerName = prompt(`Enter name for player ${i + 1}:`);
-
-          playerData.push({ name: playerName });
-        }
-        Navigate("/PlayGround", { state: { players: playerData } });
+        navigate("/PlayGround", { state: { players } });
       }
     } else {
-      const playerData = [];
-      for (let i = 0; i < selectedOption; i++) {
-        const playerName = prompt(`Enter name for player ${i + 1}:`);
-
-        playerData.push({ name: playerName });
-      }
-      Navigate("/PlayGround", { state: { players: playerData } });
+      const playerData = [...Array(parseInt(selectedOption))].map(
+        (_, index) => ({ name: players[index].name })
+      );
+      navigate("/PlayGround", { state: { players: playerData } });
     }
     setPlayers([]);
   };
@@ -58,7 +56,7 @@ export default function SelectPlayerPage() {
             <button
               onClick={() => {
                 setSelectedOption(2);
-                setPlayers([...players, 2]); // Add 2 to the players array
+                setPlayers(Array(2).fill({ name: "" })); // Add 2 to the players array
               }}
               class="btn btn-primary"
               style={{
@@ -71,7 +69,7 @@ export default function SelectPlayerPage() {
             <button
               onClick={() => {
                 setSelectedOption(3);
-                setPlayers([...players, 3]); // Add 3 to the players array
+                setPlayers(Array(3).fill({ name: "" })); // Add 3 to the players array
               }}
               class="btn btn-primary"
               style={{
@@ -82,8 +80,11 @@ export default function SelectPlayerPage() {
               3 players
             </button>
             <button
-              onClick={() => setSelectedOption("custom")}
-              class="btn btn-primary"
+              onClick={() => {
+                setSelectedOption("custom");
+                setPlayers([{ name: "" }]);
+              }}
+              className="btn btn-primary"
               style={{
                 fontSize: "100px",
                 marginRight: "20px",
@@ -95,28 +96,97 @@ export default function SelectPlayerPage() {
         )}
 
         {selectedOption === "custom" && (
-          <input
-            type="number"
-            inputMode="numeric"
-            pattern="[0-9]+"
-            placeholder="players amount..."
-            onChange={(e) => setPlayers([parseInt(e.target.value)])}
-            value={players.length > 0 ? players[0] : ""}
-            onKeyUp={(e) => {
-              if (e.key === "Enter") {
-                PlayerAdder();
+          <div>
+            <input
+              type="number"
+              inputMode="numeric"
+              pattern="[0-9]+"
+              placeholder="players amount..."
+              onChange={(e) =>
+                setPlayers(Array(parseInt(e.target.value)).fill({ name: "" }))
               }
-            }}
-            style={{
-              alignItems: "center",
-              marginLeft: "10vh",
-              marginRight: "10vh",
-              minWidth: "100vh",
-              textAlign: "center",
-              fontSize: "10vh",
-              marginTop: "10px",
-            }}
-          />
+              value={players.length > 0 ? players.length : ""}
+              onKeyUp={(e) => {
+                if (e.key === "Enter") {
+                  PlayerAdder();
+                }
+              }}
+              style={{
+                alignItems: "center",
+                marginLeft: "10vh",
+                marginRight: "10vh",
+                minWidth: "100vh",
+                textAlign: "center",
+                fontSize: "10vh",
+                marginTop: "10px",
+              }}
+            />
+            {players.slice(0, players.length).map((player, index) => (
+              <div key={index}>
+                <label
+                  htmlFor={`player-${index}`}
+                  style={{
+                    fontSize: "5vh",
+                    marginRight: "10px",
+                    color: "aqua",
+                  }}
+                >
+                  Player {index + 1} name:
+                </label>
+                <input
+                  type="text"
+                  id={`player-${index}`}
+                  placeholder={`Player ${index + 1} name...`}
+                  style={{
+                    fontSize: "5vh",
+                    padding: "10px",
+                    marginBottom: "10px",
+                    marginTop: "10px",
+                  }}
+                  value={player.name}
+                  onChange={(e) => handleNameChange(index, e)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+
+        {selectedOption !== "custom" && selectedOption !== null && (
+          <div>
+            {Array(selectedOption)
+              .fill()
+              .map((_, index) => (
+                <div key={index}>
+                  <label
+                    htmlFor={`player-${index}`}
+                    style={{
+                      fontSize: "5vh",
+                      marginRight: "10px",
+                      marginBottom: "10px",
+                      color: "aqua",
+                    }}
+                  >
+                    Player {index + 1} name:
+                  </label>
+                  <input
+                    type="text"
+                    id={`player-${index}`}
+                    placeholder={`Player ${index + 1} name...`}
+                    style={{
+                      fontSize: "5vh",
+                      padding: "10px",
+                      marginBottom: "10px",
+                      marginTop: "10px",
+                    }}
+                    onChange={(e) => {
+                      const updatedPlayers = [...players];
+                      updatedPlayers[index] = { name: e.target.value };
+                      setPlayers(updatedPlayers);
+                    }}
+                  />
+                </div>
+              ))}
+          </div>
         )}
 
         <br />

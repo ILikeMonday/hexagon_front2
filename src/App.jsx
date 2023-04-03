5aae503f91ed32d93c9a805a101a255d2755e27c
nunu111 <nunu111za@hotmail.com>
2023-04-03 16:19:51 +0700

    remove Players fromApp.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import HomePage from "./Page/HomePage";
 import SelectPlayerPage from "./Page/SelectPlayerPage";
 import HowToPlayPage from "./Page/HowToPlayPage";
 import PlayGround from "./Page/PlayGround";
-import Players from "./component/Players";
+
 import Wait_room from "./component/Wait_room";
 import Victory from "./component/Victory";
 
-window.ip = "ws://localhost:8080/demo-websocket";
+window.ip = "ws://10.83.236.206:8080/demo-websocket";
 
 export default function App() {
   return (
@@ -19,7 +19,7 @@ export default function App() {
       <Route path="/Player" element={<SelectPlayerPage />} />
       <Route path="/How-To-Play" element={<HowToPlayPage />} />
       <Route path="/Playground" element={<PlayGround />} />
-      <Route path="/namePlayer" element={<Players />} />
+
       <Route path="/wait_room" element={<Wait_room />} />
       <Route path="/Victory" element={<Victory />} />
     </Routes>
